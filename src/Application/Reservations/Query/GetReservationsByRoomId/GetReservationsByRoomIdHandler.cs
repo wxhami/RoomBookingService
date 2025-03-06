a13fcfda58d765d47645eb05649390d40619b901
Диана Агапова <d18102002@mail.ru>
2025-03-06 17:41:25 +0300

    added getReservationsByRoomIdQuery

---
+++ src/Application/Reservations/Query/GetReservationsByRoomId/GetReservationsByRoomIdHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Reservations.Query.GetReservationsByRoomId;
+
+public class GetReservationsByRoomIdHandler(IDatabaseContext databaseContext):IRequestHandler<GetReservationsByRoomIdQuery, List<Reservation>>
+{
+    public async Task<List<Reservation>> Handle(GetReservationsByRoomIdQuery request, CancellationToken cancellationToken)
+    {
+        var room = await databaseContext.Rooms.FirstOrDefaultAsync(x => x.Id == request.RoomId, cancellationToken);
+        if (room == default) throw new ObjectNotFoundException();
+
+        var reservations = databaseContext.Reservations.Where(x => x.RoomId == request.RoomId).ToList();
+
+        return reservations;
+    }
+}
---
+++ src/Application/Reservations/Query/GetReservationsByRoomId/GetReservationsByRoomIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Reservations.Query.GetReservationsByRoomId;
+
+public class GetReservationsByRoomIdQuery:IRequest<List<Reservation>>
+{
+    public Guid RoomId { get; set; }
+}
