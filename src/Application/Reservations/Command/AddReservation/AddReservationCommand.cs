db6ced5361dd92720c1253f7caa9ceef0b24df4f
Диана Агапова <d18102002@mail.ru>
2025-03-06 17:37:09 +0300

    added addCommand for reservation

---
+++ src/Application/Reservations/Command/AddReservation/AddReservationCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Application.Reservations.Command.AddReservation;
+
+public class AddReservationCommand:IRequest<Guid>
+{
+    public Guid RoomId { get; set; }
+    public Guid OrganizerId { get; set; }
+    public DateTime StartReservation { get; set; }
+    public DateTime EndReservation { get; set; }
+    public string? Description { get; set; }
+}
---
+++ src/Application/Reservations/Command/AddReservation/AddReservationHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Reservations.Command.AddReservation;
+
+public class AddReservationHandler(IDatabaseContext databaseContext): IRequestHandler<AddReservationCommand, Guid>
+{
+    public async Task<Guid> Handle(AddReservationCommand request, CancellationToken cancellationToken)
+    {
+        var reservation = new Reservation()
+        {
+            RoomId = request.RoomId,
+            OrganizerId = request.OrganizerId,
+            StartReservation = request.StartReservation,
+            EndReservation = request.EndReservation,
+            EventDescription = request.Description
+        };
+
+        databaseContext.Reservations.Add(reservation);
+        await databaseContext.SaveChangesAsync(cancellationToken);
+
+        return reservation.Id;
+    }
+}
