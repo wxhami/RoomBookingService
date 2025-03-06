743568cfa3781f980a416880ee2d81b4b5c7b464
Диана Агапова <d18102002@mail.ru>
2025-03-06 17:36:27 +0300

    added addCommand for amenity

---
+++ src/Application/Amenities/Command/AddAmenity/AddAmenityCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+using MediatR;
+
+namespace Application.Amenities.AddAmenity;
+
+public class AddAmenityCommand: IRequest<Guid>
+{
+    public string Name { get; set; } = null!;
+}
