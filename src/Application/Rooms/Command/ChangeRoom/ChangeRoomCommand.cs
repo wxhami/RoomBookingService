463bc5726b2fb3f774a43c8ad5f6f20408ac67e9
Диана Агапова <d18102002@mail.ru>
2025-03-06 17:38:21 +0300

    added changeCommands for reservation and room

---
+++ src/Application/Rooms/Command/ChangeRoom/ChangeRoomCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Rooms.Command.ChangeRoom;
+
+public class ChangeRoomCommand: IRequest
+{
+    public Guid RoomId { get; set; }
+    public string? NewName { get; set; }
+    public int? NewCapacity { get; set; }
+    public Amenity[]? NewAmenities { get; set; }
+}
