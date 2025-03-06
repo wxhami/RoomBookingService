4cbd43ae203cfca45e5917c4b601f148bc32ccc8
Диана Агапова <d18102002@mail.ru>
2025-03-06 17:40:55 +0300

    added getRoomsWithChosenAmenitiesQuery

---
+++ src/Application/Rooms/Query/GetRoomsWithChosenAmenities/GetRoomsWithChosenAmenitiesHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Rooms.Query.GetRoomsWithChosenAmenities;
+
+public class GetRoomsWithChosenAmenitiesHandler(IDatabaseContext databaseContext): IRequestHandler<GetRoomsWithChosenAmenitiesQuery, List<Room>>
+{
+    public async Task<List<Room>> Handle(GetRoomsWithChosenAmenitiesQuery request, CancellationToken cancellationToken)
+    {
+        var rooms = await databaseContext.Rooms
+            .Where(x => x.Amenities.Any(a => request.AmenitiesIds.Contains(a.Id)))
+            .ToListAsync(cancellationToken);
+
+        return rooms;
+    }
+}
---
+++ src/Application/Rooms/Query/GetRoomsWithChosenAmenities/GetRoomsWithChosenAmenitiesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Rooms.Query.GetRoomsWithChosenAmenities;
+
+public class GetRoomsWithChosenAmenitiesQuery:IRequest<List<Room>>
+{
+    public List<Guid> AmenitiesIds { get; set; } = [];
+}
