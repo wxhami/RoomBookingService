9ce16e31ee82617efb7c8d5f32924de0e8b497ac
Диана Агапова <d18102002@mail.ru>
2025-03-06 17:37:34 +0300

    added addCommand for room

---
+++ src/Application/Rooms/Command/AddRoom/AddRoomCommand.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Rooms.Command.AddRoom;
+
+public class AddRoomCommand:IRequest<Guid>
+{
+    public string Name { get; set; } = null!;
+    public int Capacity { get; set; }
+    public Amenity[] Amenities { get; set; } = [];
+}
