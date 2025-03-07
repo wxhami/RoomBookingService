65b777d80fd5369f0c3ecc32bac204aec7da8d47
Диана Агапова <d18102002@mail.ru>
2025-03-07 17:16:50 +0300

    added getAllUsersQuery

---
+++ src/Application/Users/Query/GetAllUsers/GetAllUsersHandler.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Users.Query.GetAllUsers;
+
+public class GetAllUsersHandler(UserManager<ApplicationUser> userManager):IRequestHandler<GetAllUsersQuery, List<ApplicationUser>>
+{
+    public async Task<List<ApplicationUser>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        var pageNumber = request.PageNumber > 0 ? request.PageNumber : 1;
+        var pageSize = request.PageSize > 0 ? request.PageSize : 10;
+        
+        var users = await userManager.Users
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+        
+        return users;
+    }
+}
---
+++ src/Application/Users/Query/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Persistence;
+using MediatR;
+
+namespace Application.Users.Query.GetAllUsers;
+
+public class GetAllUsersQuery:IRequest<List<ApplicationUser>>
+{
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+}
