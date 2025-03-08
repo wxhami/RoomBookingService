60140baaddbdb465dbed1c7bba7f8097274028cc
Диана Агапова <d18102002@mail.ru>
2025-03-08 14:05:19 +0300

    added usersEndpoints

---
+++ src/Client/Endpoints/UsersEndpoints.cs
@@ -0,0 +1,57 @@
+﻿using Application.Users.Command.ChangeUser;
+using Application.Users.Command.DeleteUser;
+using Application.Users.Query.GetAllUsers;
+using Application.Users.Query.GetUserById;
+using Client.Endpoints.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RouteGroupBuilder = Microsoft.AspNetCore.Routing.RouteGroupBuilder;
+
+namespace Client.Endpoints;
+
+public static class UsersEndpoints
+{
+    public static RouteGroupBuilder MapUsers(this RouteGroupBuilder group) =>
+        group.MapGroup("users", MapEndpoints);
+
+    private static void MapEndpoints(RouteGroupBuilder g)
+    {
+        g.MapGet(
+            "",
+            async (string id, [FromServices] ISender sender, CancellationToken cancellationToken)=>
+            {
+                var query = new GetUserByIdQuery() { Id = id };
+                var result = await sender.Send(query, cancellationToken);
+
+                return Results.Ok(result);
+            }
+        ).WithSummary("Получить пользователя по id");
+        
+        g.MapGet(
+            "all-users",
+            async (int? pageNumber, int? pageSize, [FromServices] ISender sender, CancellationToken cancellationToken)=>
+            {
+                var query = new GetAllUsersQuery() { PageSize = pageSize, PageNumber = pageNumber };
+                var result = await sender.Send(query, cancellationToken);
+
+                return Results.Ok(result);
+            }
+        ).WithSummary("Получить список пользователей");
+        
+        g.MapPut(
+                "",
+                async (string userId, string? newPhoneNumber, string? newName, string? newEmail, [FromServices] ISender sender, CancellationToken cancellationToken) =>
+                {
+                    await sender.Send(new ChangeUserCommand(){UserId = userId, NewEmail = newEmail, NewName = newName, NewPhoneNumber = newPhoneNumber}, cancellationToken);
+                })
+            .WithSummary("Изменить пользователя");
+        
+        g.MapDelete(
+                "",
+                async (string userId, [FromServices] ISender sender, CancellationToken cancellationToken) =>
+                {
+                    await sender.Send(new DeleteUserCommand(){UserId = userId}, cancellationToken);
+                })
+            .WithSummary("Удалить пользователя");
+    }
+}
