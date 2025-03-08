c5d83f5bcd0d265325bfbed2b28a14082133304c
Диана Агапова <d18102002@mail.ru>
2025-03-08 14:04:20 +0300

    added extension for endpoint + amenitiesEndpoints

---
+++ src/Client/Endpoints/AmenitiesEndpoints.cs
@@ -0,0 +1,39 @@
+﻿using Application.Amenities.AddAmenity;
+using Application.Amenities.Command.DeleteAmenity;
+using Application.Amenities.Query.GetAmenityById;
+using Client.Endpoints.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using AddAmenityCommand = Application.Amenities.AddAmenity.AddAmenityCommand;
+
+namespace Client.Endpoints;
+
+public static class AmenitiesEndpoints
+{
+    public static RouteGroupBuilder MapAmenities(this RouteGroupBuilder group) =>
+        group.MapGroup("amenities", MapEndpoints);
+
+    private static void MapEndpoints(RouteGroupBuilder g)
+    {
+        g.MapGet(
+            "",
+                async (Guid id, [FromServices] ISender sender, CancellationToken cancellationToken)=>
+                {
+                    var result = await sender.Send(new GetAmenityByIdQuery(){Id = id}, cancellationToken);
+
+                    return Results.Ok(result);
+                }
+        ).WithSummary("Получить название опций комнаты по id");
+        
+        g.MapPost(
+                "",
+                async (string name,  [FromServices] ISender sender, CancellationToken cancellationToken) => await sender.Send(new AddAmenityCommand(){Name = name}, cancellationToken))
+            .WithSummary("Создать опцию комнаты");
+        
+        g.MapDelete(
+                "",
+                async (Guid id, [FromServices] ISender sender, CancellationToken cancellationToken) =>
+                    await sender.Send(new DeleteAmenityCommand(){Id = id}, cancellationToken))
+            .WithSummary("Удалить опцию комнаты");
+    }
+}
---
+++ src/Client/Endpoints/Extensions/EndpointsRouteGroupBuilderExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.Endpoints.Extensions;
+
+public static class EndpointsRouteGroupBuilderExtensions
+{
+    public static void MapEndpoints(this IEndpointRouteBuilder builder) =>
+        builder.MapGroup("api/v1").MapRooms().MapAmenities().MapUsers().MapReservations();
+}
